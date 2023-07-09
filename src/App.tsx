b5af3a2592e2b3d89b21adc55b73ff8ab9c00809
Ciensprog <jorg.leonrd@gmail.com>
2023-07-09 15:40:54 -0600

    feat: add main heading section

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,12 @@ export function App() {
   return (
     <>
       <main className="my-10 z-10">
-        <div className="bg-white overflow-hidden rounded-lg">
+        <section className="bg-white overflow-hidden rounded-lg">
+          <h1 className="sr-only">List of vehicles available</h1>
           <ArticleCard baseColor="#E28625" title="Sedans" />
           <ArticleCard baseColor="#006971" title="Suvs" />
           <ArticleCard baseColor="#004140" title="Luxury" />
-        </div>
+        </section>
       </main>
 
       <Attribution />
